a47702ad42a3651ed3c9e594c6a7a87c48ae64f0
Rakesh-sensen <98682512+Rakesh-sensen@users.noreply.github.com>
2022-05-01 20:45:32 +0530

    Add files via upload

---
+++ zignan_dash.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import streamlit as st
+
+
+
+col1,col2,col3=st.columns([4,5,3])
+with col2:
+     st.title('''Zignan AI''')
+with col1:
+    pass
+with  col3:
+    pass
+
+idd='1bsWbyZbw0nZzH5XwwjTB3l88gZBhKE1WQ_iDDk1IaWs'
+#idd1=st.sidebar.text_input("ENTER THE Sheet ID",'1bsWbyZbw0nZzH5XwwjTB3l88gZBhKE1WQ_iDDk1IaWs')
+df=pd.read_csv(f"https://docs.google.com/spreadsheets/d/{idd}/export?format=csv")
+st.dataframe(df)
